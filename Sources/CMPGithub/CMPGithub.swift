b47607ddcc1e3a2f9c0c83c6d2943915bb528297
MedAnypli <mebensalem@anypli.com>
2023-02-28 11:20:38 +0100

    add the swift package

---
+++ Sources/CMPGithub/CMPGithub.swift
@@ -0,0 +1,6 @@
+public struct CMPGithub {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/CMPGithubTests/CMPGithubTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CMPGithub
+
+final class CMPGithubTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CMPGithub().text, "Hello, World!")
+    }
+}
