e27790f12d5d880a2bdced671876b5a5ebe77659
MedAnypli <mebensalem@anypli.com>
2023-02-28 11:18:04 +0100

    add the CMP XCframework

---
+++ MAdvertiseCMP.xcframework/ios-arm64_x86_64-simulator/MAdvertiseCMP.framework/Headers/MAdvertiseCMP.h
@@ -0,0 +1,18 @@
+//
+//  MAdvertiseCMP.h
+//  MAdvertiseCMP
+//
+//  Created by Hussein Dimessi on 11/16/20.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MAdvertiseCMP.
+FOUNDATION_EXPORT double MAdvertiseCMPVersionNumber;
+
+//! Project version string for MAdvertiseCMP.
+FOUNDATION_EXPORT const unsigned char MAdvertiseCMPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MAdvertiseCMP/PublicHeader.h>
+
+
